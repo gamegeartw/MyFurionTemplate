526c74443f9e15fa51ef1bb9c0d0f2e2ace8ab0c
wilber.tsai <wilber.tsai@nafco.com.tw>
2022-12-30 14:23:34 +0800

    完成側邊選單的動態產生機制

--- MyTemplate/MyTemplate.Application/System/SystemService.cs
+++ MyTemplate/MyTemplate.Application/System/SystemService.cs
@@ -1,10 +1,10 @@
-﻿using Furion.JsonSerialization;
-using Microsoft.AspNetCore.Html;
+﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MyTemplate.Core.Models;
 
 namespace MyTemplate.Application;
 
+/// <inheritdoc />
 public class SystemService : ISystemService, ITransient
 {
     private readonly IRepository<SysMenu> _sysMenuRepository;
@@ -17,26 +17,70 @@ public class SystemService : ISystemService, ITransient
         _sysMenuRepository = sysMenuRepository;
         _logger = logger;
     }
-    public string GetDescription()
-    {
-        return "让 .NET 开发更简单，更通用，更流行。";
-    }
-
+    
     /// <summary>
     /// 取得側邊選單
     /// </summary>
-    /// <param name="activeUrl"></param>
     /// <returns></returns>
-    public Task<List<MenuItemModel>> GetMenuItemsAsync(string activeUrl)
+    public Task<MenuItemModel[]> GetMenuItemsAsync()
     {
-        var result = new List<MenuItemModel>();
-        // TODO 取得側邊選單資料, 並設定 activeUrl
+        if (App.WebHostEnvironment.IsDevelopment())
+        {
+            return Task.FromResult(GetTestModel().ToArray());
+        }
 
-        var menus = _sysMenuRepository.Entities.ProjectToType<MenuItemModel>().ToList();
-        
-        return Task.FromResult(menus);
+        return Task.FromResult(_sysMenuRepository.Entities.ProjectToType<MenuItemModel>().ToArray());
+
     }
-    
+
+    /// <summary>
+    /// 假的選單資料
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerable<MenuItemModel> GetTestModel()
+    {
+        return new List<MenuItemModel>
+        {
+            new()
+            {
+                Title = "首頁",
+                Icon = "fa fa-home",
+                Url = "/Admin/main",
+            },
+            new()
+            {
+                Title = "測試Group",
+                Icon = "fa fa-list",
+                Url = "#",
+                SubMenuItems = new List<MenuItemModel>
+                {
+                    new()
+                    {
+                        Title = "測試頁面",
+                        Icon = "fa fa-circle-o",
+                        Url = "/Admin/Main2",
+                    },
+                    new()
+                    {
+                        Title = "測試第二層選單",
+                        Icon = "fa fa-list",
+                        Url = "#",
+                        SubMenuItems = new[]
+                        {
+                            new MenuItemModel
+                            {
+                                Title = "測試頁面2",
+                                Icon = "fa fa-circle-o",
+                                Url = "/Admin/Main3",
+                            }
+                        }
+                    },
+                }
+
+            }
+        };
+    }
+
     public Task<UserModel> Login(string account, string password)
     {
         var result = new UserModel();
---
+++ MyTemplate/MyTemplate.Web.Entry/Pages/Admin/Main2.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyTemplate.Web.Entry.Pages.Admin;
+
+public class Main2 : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
